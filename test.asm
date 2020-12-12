fa3feb662f51fa07acd1fd4a7be68314584e6c50
Mostafa-wael <56788883+Mostafa-wael@users.noreply.github.com>
2020-12-12 02:15:48 +0200

    added more comments

--- test.asm
+++ test.asm
@@ -1,137 +1,136 @@
 .model small
+;///////////////////////////////Data Initializations////////////////////////////////////
 .data
-	Color    equ 0fh
-	SnakSize equ 100                  	;snake's size, the snake consists of snakeSize pixels
-	originX  equ 10
-	originY  equ 10
-	originC  equ 0h
-	snakeX   DW  SnakSize dup(originX)	;the initial x-coardinates of the pixels
-	snakeY   DW  SnakSize dup(originY)	;the initialy-coardinates of the pixels
-	snakeC   DB  SnakSize dup(originC)  ;the initial color of the pixels 
-    ;since we are using words, we will use the value '2' to traverse pixels
+	Color      equ 0fh
+	paddleSize equ 100                    	;paddle's size, the paddle consists of paddleSize pixels
+	originX    equ 10
+	originY    equ 10
+	originC    equ 0fh
+	paddleX    DW  paddleSize dup(originX)	;the initial x-coardinates of the pixels
+	paddleY    DW  paddleSize dup(originY)	;the initialy-coardinates of the pixels
+	paddleC    DB  paddleSize dup(originC)	;the initial color of the pixels
+	;since we are using words, we will use the value '2' to traverse pixels
+;///////////////////////////////Data Initializations////////////////////////////////////
 .code
 MAIN PROC FAR
-	          mov  AX,@data         	;initializing the data segemnt
-	          mov  DS,AX
+	           mov  AX,@data          	;initializing the data segemnt
+	           mov  DS,AX
 
-	          mov  ah,0             	;entering the graphics mode
-	          mov  al,13h
-	          int  10h
+	           mov  ah,0              	;entering the graphics mode
+	           mov  al,13h
+	           int  10h
+	;//////////////////////////////initializing the paddle////////////////////////////////////
+	; responsiple for drawing the initial position of the paddle
+	           mov  CX,0
+	           mov  BX,0
+	fill:                             	;as all pixels have the same x & y cooardinates, this loop is responsible for giving each pixel its right coardinates
+	           add  paddleY[BX],CX
+	           add  BX,2
+	           inc  cx
+	           cmp  cx,paddleSize-1
+	           jnz  fill
 
-	          mov  CX,0
-	          mov  BX,0
+	           call Drawpaddle        	;this subroutine is responsible for drawing the paddle using its cooardinates
+	;//////////////////////////////Interacting with the user////////////////////////////////////
+	CHECK:     mov  ah,1
+	           int  16h
+	           jz   CHECK             	; check if there is any input
 
-	fill:                           	;as all pixels have the same x & y cooardinates, this loop is responsible for giving each pixel its right coardinates
-	          add  snakeY[BX],CX
-	          add  BX,2
-	          inc  cx
-	          cmp  cx,SnakSize-1
-	          jnz  fill
+	           mov  SI,offset paddleX
+	           mov  DI,offset paddleY
+	           mov  cx, 0
 
-	;this subroutine is responsible for drawing the snake using its cooardinates
-	          call DrawSnak
-	;///////////////////////Interacting with the user////////////////////////////////////
-	CHECK:    mov  ah,1
-	          int  16h
-	          jz   CHECK
+	           cmp  ah,72
+	           jz   MoveUp
 
-	          mov  SI,offset snakeX
-	          mov  DI,offset snakeY
-	          mov  cx, 0
+	           cmp  ah,80
+	           jz   MoveDown
 
-	          cmp  ah,72
-	          jz   MoveUp
+	           cmp  ah,75
+	           jz   MoveLeft
 
-	          cmp  ah,80
-	          jz   MoveDown
+	           cmp  ah,77
+	           jz   MoveRight
 
-	          cmp  ah,75
-	          jz   MoveLeft
+	ReadKey:                          	; get the pressed key from the user
+	           mov  ah,0              	;wait for a key to be pressed and put it in ah, ah:al = scan code: ASCII code
+	           int  16h
 
-	          cmp  ah,77
-	          jz   MoveRight
+	           mov  cx, 0             	; initialize cx to use it to iterate over the paddleSize
+	           call Drawpaddle
+	           jmp  CHECK
+;///////////////////////////////////////////////////////////////////////////////////////
+	MoveUp:                    
+	           inc  cx
+	           mov  BX,[DI]
+	           dec  BX                	;decrement y
+	           mov  [DI],BX
+	           add  DI, 2
+	           cmp  cx,paddleSize     	; do this for all the pixels of the paddle
+	           jnz  MoveUp
 
-	ReadKey:  
-	          mov  ah,0
-	          mov  cx, 0
-	          int  16h
-	          call DrawSnak
-	          jmp  CHECK
+	           jmp  ReadKey
 
-	MoveUp:   
-	          inc  cx
-	          mov  BX,[DI]          	;decrement y
-	          dec  BX
-	          mov  [DI],BX
-	          add  DI, 2
-	          cmp  cx,SnakSize
-	          jnz  MoveUp
+	MoveDown:  
+	           inc  cx
+	           mov  BX,[DI]
+	           inc  BX                	;increment y
+	           mov  [DI],BX
+	           add  DI, 2
+	           cmp  cx,paddleSize     	;do this for all the pixels of the paddle
+	           jnz  MoveDown
 
-	          jmp  ReadKey
+	           jmp  ReadKey
 
-	MoveDown: 
-	          inc  cx
-	          mov  BX,[DI]          	;increment y
-	          inc  BX
-	          mov  [DI],BX
-	          add  DI, 2
-	          cmp  cx,SnakSize
-	          jnz  MoveDown
-
-	          jmp  ReadKey
-
-	MoveLeft: 
-	          inc  cx
-	          mov  BX,[SI]          	;decrement x
-	          dec  BX
-	          mov  [SI],BX
-	          add  SI, 2
-	          cmp  cx,SnakSize
-	          jnz  MoveLeft
+	MoveLeft:  
+	           inc  cx
+	           mov  BX,[SI]
+	           dec  BX                	;decrement x
+	           mov  [SI],BX
+	           add  SI, 2
+	           cmp  cx,paddleSize     	;do this for all the pixels of the paddle
+	           jnz  MoveLeft
 	          
-	          jmp  ReadKey
+	           jmp  ReadKey
 
-	MoveRight:
-	          inc  cx
-	          mov  BX,[SI]          	;increment x
-	          inc  BX
-	          mov  [SI],BX
-	          add  SI, 2
-	          cmp  cx,SnakSize
-	          jnz  MoveRight
+	MoveRight: 
+	           inc  cx
+	           mov  BX,[SI]
+	           inc  BX                	;increment x
+	           mov  [SI],BX
+	           add  SI, 2
+	           cmp  cx,paddleSize     	;do this for all the pixels of the paddle
+	           jnz  MoveRight
 
-	          jmp  ReadKey
-	;/////////////////////////////////////////////////////////////////////////////////////////
+	           jmp  ReadKey
+	;///////////////////////////////////////////////////////////////////////////////////////
 
- 
-
-	          HLT
+	           HLT
 MAIN ENDP
+	;//////////////////////////////Procedures////////////////////////////////////
+Drawpaddle PROC
 
-DrawSnak PROC
+	           mov  ah,0              	;entering the graphics mode, to clear screen
+	           mov  al,13h
+	           int  10h
+	; initialize containers
+	           mov  SI, offset paddleX
+	           mov  DI, offset paddleY
+	           mov  bp, offset paddleC
+	           mov  Bl,paddleSize
 
-	          mov  ah,0             	;entering the graphics mode, to clear screen
-	          mov  al,13h
-	          int  10h
-
-	          mov  SI,offset snakeX
-	          mov  DI,offset snakeY
-	          mov  Bl,SnakSize
-	          mov  bp, offset snakeC
-
-
-	          mov  ah,0ch           	;Draw Pixel Command
-	back:     
-	          mov  al,[bp]          	;Pixel color
-	          mov  cx,[SI]          	;Column
-	          mov  dx,[DI]          	;Row
-	          int  10h
-	          add  SI,2
-	          add  DI,2
-	;add  bp, 1
-	          dec  bl
-	          jnz  back
-	          ret
-DrawSnak ENDP
- 
+	           mov  ah,0ch            	;Draw Pixel Command
+	back:      
+	           mov  al,[bp]           	;Pixel color
+	           mov  cx,[SI]           	;Column
+	           mov  dx,[DI]           	;Row
+	           int  10h               	;draw the pixel
+	           add  SI,2              	;move to the next paddleX
+	           add  DI,2              	;move to the next paddleY
+	           dec  bl
+	;add  bp, 1 ;adding differnet colors to each pixel
+	           jnz  back              	;loop over the paddle size
+	           ret
+Drawpaddle ENDP
+;///////////////////////////////////////////////////////////////////////////////////////
         END MAIN
